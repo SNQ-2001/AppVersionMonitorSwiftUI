598c49c2e7d52031a86ccd7e77f7f6e7fe80f7a7
SNQ-2001 <taishin05109@gmail.com>
2023-02-24 01:16:03 +0900

    feat: create package

---
+++ Tests/AppVersionMonitorSwiftUITests/AppVersionMonitorSwiftUITests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import AppVersionMonitorSwiftUI
+
+final class AppVersionMonitorSwiftUITests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(AppVersionMonitorSwiftUI().text, "Hello, World!")
+    }
+}
