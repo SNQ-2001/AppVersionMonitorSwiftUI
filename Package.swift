f113facc5f92275ae14c701b1d8257a49b6582ed
SNQ-2001 <taishin05109@gmail.com>
2023-02-24 21:05:06 +0900

    feat: platformsを追加

--- Package.swift
+++ Package.swift
@@ -6,7 +6,10 @@ import PackageDescription
 let package = Package(
     name: "AppVersionMonitorSwiftUI",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v13),
+        .macOS(.v10_13),
+        .tvOS(.v11),
+        .watchOS(.v5)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
